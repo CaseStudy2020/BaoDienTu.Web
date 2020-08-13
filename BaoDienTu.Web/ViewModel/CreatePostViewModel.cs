d1e6ef0eb3742e525d439afa72cb07f6c15993be
Nguyen HuynhThao <tfosorcim1994@outlook.com>
2020-08-14 06:42:14 +0700

    NguyenHT-AddAuthorIndex

---
+++ BaoDienTu.Web/ViewModel/CreatePostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaoDienTu.Web.ViewModel
+{
+    public class CreatePostViewModel
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+        public string link { get; set; }
+        public string authorId { get; set; }
+        public string thumbnail { get; set; }
+        public int averageRate { get; set; }
+    }
+}
