f3d0c76a9ff9aef4f5d25ff3d1f9291fd690b135
cuong <xuanc25@gmail.com>
2020-08-14 07:23:21 +0700

    cuong dev

--- BaoDienTu.Web/Models/Post/UpdatePostResult.cs
+++ BaoDienTu.Web/Models/Post/UpdatePostResult.cs
@@ -9,17 +9,7 @@ namespace BaoDienTu.Web.Models.Post
     public class UpdatePostResult
     {
         public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public int Like { get; set; }
-        public string Link { get; set; }
-        public Status Status { get; set; }
-        public int CategoryId { get; set; }
-        public int PostHashTagId { get; set; }
-        public int AuthorId { get; set; }
-        public int SubCategoryId { get; set; }
-        public int View { get; set; }
-        public string DateCreated { get; set; }
-        public decimal AverageRate { get; set; }
+     
+       public string Message { get; set; }
     }
 }
